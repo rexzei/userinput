6d1a91158a34e515759b9576e1c635444913c115
Arvid Nordström <rex07076@gmail.com>
2018-06-23 22:25:09 +0200

    added character spelling

--- whatistheword.py
+++ whatistheword.py
@@ -13,6 +13,7 @@ attempts = 0
 
 # Array
 wordArray = ["apple", "orange", "melon", "kiwi"]
+colorArray = ["red", "orange", "yellow", "green"]
 
 # Get lenght of array
 wordArrayLengt = len(wordArray)
@@ -21,8 +22,16 @@ wordArrayLengt = len(wordArray)
 wordArrayNumber = random.randrange(0, wordArrayLengt)
 wordArrayPrint = wordArray[wordArrayNumber]
 
+# Match colorArray with wordArray
+colorArrayPrint = colorArray[wordArrayNumber]
+
 # Get lenght of random string
 wordArrayPrintLenght = len(wordArrayPrint)
+
+# Get first character of random string
+wordArrayPrintOneCharacter = wordArrayPrint[:1]
+wordArrayPrintTwoCharacter = wordArrayPrint[:2]
+wordArrayPrintThreeCharacter = wordArrayPrint[:3]
 
 # Defining attemptsLeft as a string
 attemptsLeftToString = str(attemptsLeft)
@@ -32,10 +41,13 @@ attemptsLeftPhrase = "You have " + attemptsLeftToString + " attempts left"
 attemptsToString = str(wordArrayPrintLenght)
 attemptsPhrase1 = "The word has " + attemptsToString + " characters"
 
+# colorArray into a phrase
+colorArrayPhrase = "The color can be " + colorArrayPrint
+
 print(wordArrayPrint)
 print(wordArrayPrintLenght)
 
-# Get userinput to userInput variable
+# User Input
 while answer == 0:
     userInput = input("The word is: ")
     attemptsLeft = attemptsLeft - 1
@@ -58,5 +70,13 @@ while answer == 0:
         answer = 0
         if attempts == 1:
             print(attemptsPhrase1)
+        elif attempts == 2:
+            print(wordArrayPrintOneCharacter)
+        elif attempts == 3:
+            print(wordArrayPrintTwoCharacter)
+        elif attempts == 4:
+            print(wordArrayPrintThreeCharacter)
+        elif attempts == 5:
+            print(colorArrayPhrase)
 
 print("Game end")
